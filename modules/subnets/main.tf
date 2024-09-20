d7a615b531be1454a86b9f8aa678492ebacc0f2f
vaibhavwagh9119 <118240657+vaibhavwagh9119@users.noreply.github.com>
2024-09-20 16:00:39 +0530

    Update main.tf

--- modules/subnets/main.tf
+++ modules/subnets/main.tf
@@ -5,17 +5,9 @@ resource "azurerm_subnet" "stere" {
   address_prefixes     = ["10.240.0.0/16"]
 }
 
-resource "azurerm_subnet" "biqmind" {
-  name                 = "biqmind"
-  resource_group_name  = var.resource_group_name
-  virtual_network_name = var.virtual_network_name
-  address_prefixes     = ["10.241.0.0/16"]
-}
+
 
 output "stere_subnet_id" {
   value = azurerm_subnet.stere.id
 }
 
-output "biqmind_subnet_id" {
-  value = azurerm_subnet.biqmind.id
-}
